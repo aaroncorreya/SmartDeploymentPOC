eccd5efa237a2d8e1da187b7b57e6c037d4507da
Aaron Correya <aaroncorreya@microsoft.com>
2021-11-12 16:53:03 -0800

    Created rough draft of script with API functionality

--- script.ps1
+++ script.ps1
@@ -1,5 +1,9 @@
 $global:outfile="trackingTable.csv"
 
+# Param (
+#     [Parameter(Mandatory=$true)][string]$GithubToken
+# )
+
 function add-csv {
     if (!(Test-Path $outfile)) {
         $newcsv = {} | Select "FILE_NAME","SHA" | Export-Csv $outfile
@@ -8,25 +12,45 @@ function add-csv {
     }
 }
 
-function main {
-    $fileDirectory = ".\Deployments"
-    add-csv 
-    # $parse_results = New-Object System.Collections.ArrayList;
-
-    # Use a foreach to loop through all the files in a directory.
-    # This method allows us to easily track the file name so we can report 
-    # our findings by file.
-    # get current path and then add file name to it 
-    foreach($file in Get-ChildItem $fileDirectory)
-    {
-        # Processing code goes here
-        $filePath = $fileDirectory + "\" + $file;
-        $sha =  (Get-Content -Path $file | ConvertFrom-Json).sha
-
-        Write-Output $sha 
-
-        "{0},{1}" -f $filePath,$sha | add-content -path $outfile
+function main { 
+    $token = "ghu_3EHLFRevI3QYc80494jfsilN6tQ4rI1vJUOL"
+    $Header = @{
+        "authorization" = "Bearer $token"
     }
+
+    $branchResponse = Invoke-RestMethod https://api.github.com/repos/aaroncorreya/SmartDeploymentPOC/branches -Headers $header
+
+    Write-Output $branchResponse
+    Write-Output $branchResponse.GetType().Name
+
+    $mainSha = $branchResponse | ForEach-Object -Process {if ($_.name -eq "main") {$_.commit.sha}}
+
+    Write-Output $mainSha
+
+    $treeUrl = "https://api.github.com/repos/aaroncorreya/SmartDeploymentPOC/git/trees/" + $mainSha + "?recursive=true"
+    Write-Output $treeUrl
+    $treeResponse = Invoke-RestMethod $treeUrl -Headers $header
+
+    Write-Output $treeResponse.GetType().Name
+    Write-Output $treeResponse.sha
+
+    #Create hashtable
+    $csvTable = @{}
+    #Add all json files to the hashtable
+    $treeResponse.tree | ForEach-Object -Process {if ($_.path.Substring($_.path.Length-5) -eq ".json") {$csvTable.Add($_.path, $_.sha)}}
+
+    Write-Output "testing dictionary"
+    Write-Output $csvTable
+
+    #Enumerate through table and add to csv file
+    add-csv
+    $csvTable.GetEnumerator() | foreach {
+        "{0},{1}" -f $_.Key, $_.Value | add-content -path $outfile
+    }
+
+    $createFileUrl = "https://api.github.com/repos/aaroncorreya/SmartDeploymentPOC/repos/aaroncorreya/SmartDeploymentPOC/contents/trackingTable.csv"
+    $createResponse = Invoke-RestMethod $createFileUrl -Headers $header
+    Write-Output $createResponse
 }
 
 main
